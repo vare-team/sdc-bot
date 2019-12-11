d04f771a7717637bbf4d3baf1e2d008f6860b66f
MegaVasiliy007 <vasiliy072000mail.ru@gmail.com>
2019-12-11 17:39:12 +0300

    disable getapikey, add ignore blacklist guild, add codestyle, add new lib

--- commands/getapikey.js
+++ commands/getapikey.js
@@ -9,10 +9,10 @@ function makeid() {
 exports.help = {
 	flag: 0,
 	args: 0
-}
+};
 
 exports.run = async (client, msg) => {
-	if (Object.keys(client.userLib.admins).indexOf(msg.author.id) == -1) {
+	/*if (Object.keys(client.userLib.admins).indexOf(msg.author.id) == -1) {
 		console.log(msg.author.tag);
 		let data = (await client.userLib.promise(client.userLib.db, client.userLib.db.queryValue, 'SELECT status FROM server WHERE id = ?', [msg.guild.id])).res;
 
@@ -24,5 +24,6 @@ exports.run = async (client, msg) => {
 	let embed = new client.userLib.discord.RichEmbed().setAuthor('DEV.Key').setDescription(`Для вашего сервера **${msg.guild.name}** был сгенерирован ключ разработчика!`).addField('Ключ:', `||${temp}||`).setTimestamp().setColor('#43B581').setFooter('Никому не сообщайте этот ключ!');
 	
 	client.userLib.db.query('INSERT INTO developers (id, dKey) VALUES (?, ?) ON DUPLICATE KEY UPDATE dKey = ?', [msg.guild.id, temp, temp], () => {msg.guild.owner.send(embed)});
-// 	client.userLib.db.upsert('developers', {id: msg.guild.id, dKey: temp}, () => {msg.author.send(embed)});
-}
+ 	client.userLib.db.upsert('developers', {id: msg.guild.id, dKey: temp}, () => {msg.author.send(embed)});
+	 */
+};
--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,24 +1,27 @@
 module.exports = async (client, guild) => {
- 
-  let result = (await client.userLib.promise(client.userLib.db, client.userLib.db.count, 'server', {id: guild.id})).res;
-  if (!result) {guild.leave(); return;}
 
-  client.userLib.db.query('UPDATE server SET bot = 1, members = ?, online = ?, ownerID = ? WHERE id = ?', [guild.memberCount, guild.members.filter(m => m.presence.status == 'online' && !m.user.bot).size, guild.ownerID, guild.id], () => {});
-  
-  let embed = new client.userLib.discord.RichEmbed()
-  .setAuthor('Новый сервер!')
-  .setTitle(guild.name + ` (ID : ${guild.id})`)
-  .setThumbnail(guild.iconURL)
-  .setColor('#43B581')
-  .addField('Участников/Онлайн', `${guild.memberCount} / ${guild.members.filter(m => m.presence.status == 'online' && !m.user.bot).size}`)
-  .addField('Владелец сервера', `${guild.owner ? guild.owner.user.tag : 'NULL#0000'} (ID: ${guild.ownerID})`)
-  .addField('Регион сервера', guild.region)
-  .setTimestamp()
-  .setFooter(`Добавился`);
-  client.userLib.logc.send(embed);
+	let black = (await client.userLib.promise(client.userLib.db, client.userLib.db.query, 'SELECT COUNT(*) FROM blacklist WHERE id = ? AND warns > 2', [guild.id])).res;
+	if (black) {guild.leave(); return;}
 
-  let all = await client.shard.fetchClientValues('guilds.size');
-  all = all.reduce((prev, val) => prev + val, 0) - 1;
-  client.userLib.db.query('INSERT INTO sdcstat (date, added, guilds) VALUES (?, 1, ?) ON DUPLICATE KEY UPDATE added = added + 1, guilds = ?', [new Date, all, all]);
+	let result = (await client.userLib.promise(client.userLib.db, client.userLib.db.count, 'server', {id: guild.id})).res;
+	if (!result) {guild.leave(); return;}
+
+	client.userLib.db.query('UPDATE server SET bot = 1, members = ?, online = ?, ownerID = ? WHERE id = ?', [guild.memberCount, guild.members.filter(m => m.presence.status == 'online' && !m.user.bot).size, guild.ownerID, guild.id], () => {});
+
+	let embed = new client.userLib.discord.RichEmbed()
+		.setAuthor('Новый сервер!')
+		.setTitle(guild.name + ` (ID : ${guild.id})`)
+		.setThumbnail(guild.iconURL)
+		.setColor('#43B581')
+		.addField('Участников/Онлайн', `${guild.memberCount} / ${guild.members.filter(m => m.presence.status == 'online' && !m.user.bot).size}`)
+		.addField('Владелец сервера', `${guild.owner ? guild.owner.user.tag : 'NULL#0000'} (ID: ${guild.ownerID})`)
+		.addField('Регион сервера', guild.region)
+		.setTimestamp()
+		.setFooter(`Добавился`);
+	client.userLib.logc.send(embed);
+
+	let all = await client.shard.fetchClientValues('guilds.size');
+	all = all.reduce((prev, val) => prev + val, 0) - 1;
+	client.userLib.db.query('INSERT INTO sdcstat (date, added, guilds) VALUES (?, 1, ?) ON DUPLICATE KEY UPDATE added = added + 1, guilds = ?', [new Date, all, all]);
 
 };
