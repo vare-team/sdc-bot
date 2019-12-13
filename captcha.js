846dfd32f48855d84cc5171b8d9ba2f1d86fc508
MegaVasiliy007 <vasiliy072000mail.ru@gmail.com>
2019-12-14 02:21:06 +0300

    fix path in captcha

--- captcha.js
+++ captcha.js
@@ -6,7 +6,7 @@ module.exports = async (Discord, msgchan, msgauth, captchaType = 'image') => {
 
 	if (captchaType == "image") {
 
-		let _image = await jimp.read("/root/bots/sediscom/captcha.png");
+		let _image = await jimp.read("./captcha.png");
 		_image.resize(375, 100); // make bigger
 		_image.print(await jimp.loadFont(jimp.FONT_SANS_64_BLACK), Math.random() * 160, Math.random() * 53, captcha); // print captcha on image
 		_image.getBuffer(jimp.MIME_PNG, (err, buff) => msgchan.send('Введите код с картинки', new Discord.Attachment(buff, "captcha.png")).then(captmsg => captmsg.delete(15000).catch(() => {})) );
@@ -20,4 +20,4 @@ module.exports = async (Discord, msgchan, msgauth, captchaType = 'image') => {
 	}
 
 	return flag ? 1 : 0;
-}
+};
