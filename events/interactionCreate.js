89f3bdcd95772c422f2d1ef51ee4501869757939
dellyare <37771198+Dellyare@users.noreply.github.com>
2021-09-24 20:07:06 +0300

    Remove replies

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,19 +1,11 @@
-import messages from '../models/replies';
 import commands from '../commands';
 
 export default async function (interaction) {
 	if (!interaction.inGuild() || !interaction.isCommand()) return;
 
 	try {
-		if (!commands.hasOwnProperty(interaction.commandName)) {
-			interaction.reply({ embeds: messages[interaction.commandName].embeds }); // Заглушка для тестов
-			return;
-		}
-
 		await commands[interaction.commandName](interaction);
 	} catch (e) {
 		console.log(e);
-		// Если хочешь пиши суда какую-то фигню чтоб ловить ошибки при ожидании
-		// interaction.reply('error').catch(() => interaction.editReply('error edit'));
 	}
 }
