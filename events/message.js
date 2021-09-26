5838114b3a7ad5395838cf6a32fde8b3ea231964
nitroflap <fe.lap.prog@gmail.com>
2021-09-26 16:05:05 +0300

    refator(events/message): generic deprecated commands response

--- events/message.js
+++ events/message.js
@@ -1,18 +1,19 @@
-export default function (message) {
+function checkDeprecatedCommands(message) {
 	if (!message.content.startsWith('s.')) return;
 
-	if (message.content.includes('s.up'))
-		message.reply(
-			'Ошибка! Бот переходит на новый тип взаимодействия - слеш команды!\nДля того, что бы апнуть сервер пропишите `/up`!\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825'
-		);
+	const commands = [
+		{ name: 's.up', description: 'Для того, что бы апнуть сервер пропишите `/up`!' },
+		{ name: 's.info', description: 'Для того, что бы узнать информацию о сервере пропишите `/info`!' },
+		{ name: 's.s', description: 'Для того, что бы узнать ссылки пропишите `/links`!' }
+	]
 
-	if (message.content.includes('s.info'))
-		message.reply(
-			'Ошибка! Бот переходит на новый тип взаимодействия - слеш команды!\nДля того, что бы узнать информацию о сервере пропишите `/info`!\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825'
+	for (const command of commands) {
+		if (message.content.includes(command.name)) message.reply(
+			`Ошибка! Бот переходит на новый тип взаимодействия - слеш команды!${command.description}\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825`
 		);
+	}
+}
 
-	if (message.content.includes('s.s'))
-		message.reply(
-			'Ошибка! Бот переходит на новый тип взаимодействия - слеш команды!\nДля того, что бы узнать ссылки пропишите `/links`!\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825'
-		);
+export default function (message) {
+	checkDeprecatedCommands(message);
 }
