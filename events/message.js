30298a8c94461dd39f9030efca28d4cec2b26ab7
dellyare <37771198+Dellyare@users.noreply.github.com>
2021-10-03 12:13:50 +0300

    Фикс триггера команд в сообщениях

--- events/message.js
+++ events/message.js
@@ -1,18 +1,19 @@
 export default function (message) {
-	if (!message.content.startsWith('s.')) return;
-
-	if (message.content.includes('s.up'))
-		message.reply(
-			'Ошибка! Бот переходит на новый тип взаимодействия - слеш команды!\nДля того, что бы апнуть сервер пропишите `/up`!\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825'
-		);
-
-	if (message.content.includes('s.info'))
-		message.reply(
-			'Ошибка! Бот переходит на новый тип взаимодействия - слеш команды!\nДля того, что бы узнать информацию о сервере пропишите `/info`!\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825'
-		);
-
-	if (message.content.includes('s.s'))
-		message.reply(
-			'Ошибка! Бот переходит на новый тип взаимодействия - слеш команды!\nДля того, что бы узнать ссылки пропишите `/links`!\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825'
-		);
+	if (!message.content.toLowerCase().startsWith('s.')) return;
+	switch (message.content.toLowerCase()) {
+		case 's.up':
+			message.reply(
+				'**Внимание! Бот переходит на новый тип взаимодействия - слеш команды!**\nДля того, что бы апнуть сервер пропишите `/up`!\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825'
+			);
+			break;
+		case 's.info':
+			message.reply(
+				'**Внимание! Бот переходит на новый тип взаимодействия - слеш команды!**\nДля того, что бы апнуть сервер пропишите `/up`!\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825'
+			);
+			break;
+		case 's.s':
+			message.reply(
+				'**Внимание! Бот переходит на новый тип взаимодействия - слеш команды!**\nДля того, что бы узнать ссылки пропишите `/links`!\nОднако, если слеш-команды не появились, перепригласите бота с нужными правами!\nПриглашение: https://discord.com/oauth2/authorize?client_id=464272403766444044&scope=bot+applications.commands&permissions=3533825'
+			);
+	}
 }
