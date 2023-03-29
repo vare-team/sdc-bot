a281b83339b165570cf351ba926489beab4072a5
Alexander <satan1c@ukr.net>
2023-03-30 02:30:15 +0300

    user of undefined fix

--- utils/changePresence.js
+++ utils/changePresence.js
@@ -2,19 +2,14 @@ import { ActivityType } from 'discord.js';
 
 let presence = 1;
 
-/**
- *
- * @param client {Client}
- * @return {Promise<void>}
- */
-export default async function (client) {
+export default async function () {
 	if (presence === 1) {
-		const guilds = await client.shard.fetchClientValues('guilds.cache.size');
-		client.user.setActivity(`серверов: ${guilds.reduce((p, v) => p + v, 0)} | /up`, { type: ActivityType.Watching });
+		const guilds = await bot.shard.fetchClientValues('guilds.cache.size');
+		bot.user.setActivity(`серверов: ${guilds.reduce((p, v) => p + v, 0)} | /up`, { type: ActivityType.Watching });
 	} else if (presence === 2) {
-		client.user.setActivity('/up | /info', { type: ActivityType.Listening });
+		bot.user.setActivity('/up | /info', { type: ActivityType.Listening });
 	} else if (presence === 3) {
-		client.user.setActivity('Апнуть сервер - /up', { type: ActivityType.Listening });
+		bot.user.setActivity('Апнуть сервер - /up', { type: ActivityType.Listening });
 		presence = 0;
 	}
 
